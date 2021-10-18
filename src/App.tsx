4327951f27dc88124360d30116924d734d22ae34
Chupriarti <wallx13@gmail.com>
2021-10-18 23:11:12 +0500

    added chessboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Chessboard from 'chessboardjsx';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>React App</p>
+      <Chessboard
+        position="start"
+        width={500}
+        />
     </div>
   );
 }
