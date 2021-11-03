8b1b1354bcca9d73b2e003c7364e3f99dea9a3ee
Chupriarti <wallx13@gmail.com>
2021-11-03 22:11:57 +0300

    finished board

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -10,7 +10,12 @@ export default function Chessboard(){
 
     for (let j = verticalAxis.length - 1; j >= 0; j--){
         for (let i = 0; i < horizontalAxis.length; i++){
-            board.push(<div className="tile">[{horizontalAxis[i]}{verticalAxis[j]}]</div>)
+            let number = j + i + 2;
+            if (number % 2 === 0){
+                board.push(<div className="tile black-tile"></div>)
+            } else {
+                board.push(<div className="tile white-tile"></div>)
+            }
         }
     }
 
