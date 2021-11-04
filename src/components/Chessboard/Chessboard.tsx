e3a9d80968f8e103089ffa72f6ddc54ad83168b5
Chupriarti <wallx13@gmail.com>
2021-11-04 21:03:33 +0300

    added piece placement logic

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -5,6 +5,15 @@ import './Chessboard.css';
 const verticalAxis = ["1","2","3","4","5","6","7","8"];
 const horizontalAxis = ["a","b","c","d","e","f","g", "h"];
 
+interface Piece {
+    image: string
+    x: number
+    y: number
+}
+
+const pieces: Piece[] = [];
+
+pieces.push({image: "assets/images/pawn_b.png", x: 0, y: 6})
 
 export default function Chessboard(){
     let board = [];
@@ -12,7 +21,13 @@ export default function Chessboard(){
     for (let j = verticalAxis.length - 1; j >= 0; j--){
         for (let i = 0; i < horizontalAxis.length; i++){
             let number = j + i + 2;
-            board.push(<Tile number={number} />)
+            let image = undefined;
+            pieces.forEach(p => {
+                if (p.x === i && p.y === j){
+                    image = p.image;
+                }
+            })
+            board.push(<Tile number={number} image={image} />)
         }
     }
 
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,13 +1,14 @@
 import './Tile.css'
 
 interface TileProps {
+    image?: string;
     number: number;
 }
 
-export default function Tile({number}: TileProps){
+export default function Tile({number, image}: TileProps){
     if (number % 2 === 0){
-        return <div className="tile black-tile"></div>
+        return <div className="tile black-tile"><img src={image} /></div>
     } else {
-        return <div className="tile white-tile"></div>
+        return <div className="tile white-tile"><img src={image} /></div>
     }
 }
