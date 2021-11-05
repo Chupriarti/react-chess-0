e06a52fa5d4e4b2dd4a757ba9e3029028d5d477f
Chupriarti <wallx13@gmail.com>
2021-11-05 11:45:06 +0300

    moved initial board state to react state

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -11,31 +11,33 @@ interface Piece {
     y: number
 }
 
-const pieces: Piece[] = [];
+ const initialBoardState: Piece[] = [];
 
-for (let p = 0; p < 2; p++){
+ for (let p = 0; p < 2; p++){
     const type = p === 0 ? "b" : "w";
     const y = p === 0 ? 7 : 0;
     
-    pieces.push({image: `assets/images/rook_${type}.png`, x: 0, y})
-    pieces.push({image: `assets/images/rook_${type}.png`, x: 7, y})
-    pieces.push({image: `assets/images/knight_${type}.png`, x: 1, y})
-    pieces.push({image: `assets/images/knight_${type}.png`, x: 6, y})
-    pieces.push({image: `assets/images/bishop_${type}.png`, x: 2, y})
-    pieces.push({image: `assets/images/bishop_${type}.png`, x: 5, y})
-    pieces.push({image: `assets/images/queen_${type}.png`, x: 3, y})
-    pieces.push({image: `assets/images/king_${type}.png`, x: 4, y})
+    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 0, y})
+    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 7, y})
+    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 1, y})
+    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 6, y})
+    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 2, y})
+    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 5, y})
+    initialBoardState.push({image: `assets/images/queen_${type}.png`, x: 3, y})
+    initialBoardState.push({image: `assets/images/king_${type}.png`, x: 4, y})
 }
 
 for (let i = 0; i < 8; i++){
-    pieces.push({image: "assets/images/pawn_b.png", x: i, y: 6})
+    initialBoardState.push({image: "assets/images/pawn_b.png", x: i, y: 6})
 }
 
 for (let i = 0; i < 8; i++){
-    pieces.push({image: "assets/images/pawn_w.png", x: i, y: 1})
+    initialBoardState.push({image: "assets/images/pawn_w.png", x: i, y: 1})
 }
 
 export default function Chessboard(){
+    const [pieces, setPieces] = React.useState<Piece[]>(initialBoardState)
+
     const chessboardRef = React.useRef<HTMLDivElement>(null);
 
     let activePiece: HTMLElement | null = null;
