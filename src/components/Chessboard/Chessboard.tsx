acb19c75ca6c836c748adbd8427c9250464c035d
Chupriarti <wallx13@gmail.com>
2021-11-05 11:03:36 +0300

    added moving pieces

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -35,7 +35,30 @@ for (let i = 0; i < 8; i++){
     pieces.push({image: "assets/images/pawn_w.png", x: i, y: 1})
 }
 
+let activePiece: HTMLElement | null = null;
 
+function grabPiece(e: React.MouseEvent){
+    const element = e.target as HTMLElement;
+    if (element.classList.contains("chess-piece")){
+        activePiece = element;
+    }
+}
+
+function movePiece(e: React.MouseEvent){
+    if (activePiece){
+        const x = e.clientX - 50;
+        const y = e.clientY - 50;
+        activePiece.style.position = "absolute";
+        activePiece.style.left = `${x}px`
+        activePiece.style.top = `${y}px`
+    }
+}
+
+function dropPiece (e: React.MouseEvent){
+    if (activePiece){
+        activePiece = null;
+    }
+}
 
 export default function Chessboard(){
     let board = [];
@@ -53,5 +76,9 @@ export default function Chessboard(){
         }
     }
 
-    return <div id="chessboard">{board}</div>
+    return <div 
+        onMouseDown={e => grabPiece(e)} 
+        onMouseMove={e => movePiece(e)} 
+        onMouseUp={e => dropPiece(e)}
+        id="chessboard">{board}</div>
 }
