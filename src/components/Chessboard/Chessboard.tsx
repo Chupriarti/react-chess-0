e141bbcc9880272f86340d039b3da391dde50071
Chupriarti <wallx13@gmail.com>
2021-11-06 12:19:35 +0300

    added removing attacked piece

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -168,32 +168,40 @@ export default function Chessboard(){
         const chessboard = chessboardRef.current;
         if (activePiece && chessboard){
             const x = Math.floor((e.clientX - chessboard.offsetLeft) / 100);
-            const y = Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100));   
-            setPieces(prev => {
-                const pieces = prev.map((p) => {
-                    if (p.x === gridX && p.y === gridY) {
-                        const isValidMove = referee.isValidMove(
-                            gridX,
-                            gridY, 
+            const y = Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100));  
+            const currentPiece = pieces.find(p => p.x === gridX && p.y === gridY);
+            const attackedPiece = pieces.find(p => p.x === x && p.y === y);
+            if (currentPiece){
+                const isValidMove = referee.isValidMove(
+                    gridX,
+                    gridY, 
+                    x, 
+                    y, 
+                    currentPiece.type, 
+                    currentPiece.team, 
+                    pieces
+                );
+                const updatedPieces = pieces.reduce((results, piece) => {
+                    if (piece.x === currentPiece.x && piece.y === currentPiece.y){
+                        results.push({
+                            ...piece,
                             x, 
-                            y, 
-                            p.type, 
-                            p.team, 
-                            prev
-                        );
-                        if (isValidMove){
-                            p.x = x;
-                            p.y = y;
-                        } else {
-                            activePiece.style.position = "relative";
-                            activePiece.style.removeProperty("top");
-                            activePiece.style.removeProperty("left");
-                        }
+                            y
+                        });
+                    } else if (!(piece.x === x && piece.y === y)){
+                        results.push(piece);
                     }
-                    return p
-                })
-                return pieces;
-            })
+                    
+                    return results;
+                }, [] as Piece[]);
+                if (isValidMove){
+                    setPieces(updatedPieces)
+                } else {
+                    activePiece.style.position = "relative";
+                    activePiece.style.removeProperty("top");
+                    activePiece.style.removeProperty("left");                    
+                }
+            }
             setActivePiece(null)
         }
     }
