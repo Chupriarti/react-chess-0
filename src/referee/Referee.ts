7ff9ff4b6db8f15a759d8900db50e18402747cd7
Chupriarti <wallx13@gmail.com>
2021-11-09 21:54:48 +0300

    added other bishop move directions

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -76,6 +76,21 @@ export default class Referee {
                     return true;
                 }               
             }
+            for (let i = 0; i < 8; i++){
+                if (desiredPosition.x - initialPosition.x === -i && desiredPosition.y - initialPosition.y === i){
+                    return true;
+                }               
+            }
+            for (let i = 0; i < 8; i++){
+                if (desiredPosition.x - initialPosition.x === i && desiredPosition.y - initialPosition.y === -i){
+                    return true;
+                }               
+            }
+            for (let i = 0; i < 8; i++){
+                if (desiredPosition.x - initialPosition.x === -i && desiredPosition.y - initialPosition.y === -i){
+                    return true;
+                }               
+            }
         }
         return false;
     }
