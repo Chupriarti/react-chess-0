d451799f1af805ecc2d3f3c0b806f11791b719af
Chupriarti <wallx13@gmail.com>
2021-11-10 23:17:24 +0300

    move rook logic to function

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -126,6 +126,39 @@ export default class Referee {
         return false;
     }
 
+    rookMove (initialPosition: Position, desiredPosition: Position, team: TeamType, boardState: Piece[]): boolean{
+        if (initialPosition.x === desiredPosition.x){
+            for (let i = 1; i < 8; i++){
+                let multiplier = (desiredPosition.y < initialPosition.y) ? -1 : 1;
+                let passedPosition: Position = {x: initialPosition.x, y: initialPosition.y + (i * multiplier)};
+                if (samePosition(desiredPosition, passedPosition)){
+                    if (this.tileIsEmptyOrOccupiedByEnemy(passedPosition, boardState, team)){
+                        return true;
+                    }                       
+                } else {
+                    if (this.tileIsOccupied(passedPosition, boardState)){
+                        break;
+                    }
+                } 
+            }
+        } else if (initialPosition.y === desiredPosition.y){
+            for (let i = 1; i < 8; i++){
+                let multiplier = (desiredPosition.x < initialPosition.x) ? -1 : 1;
+                let passedPosition: Position = {x: initialPosition.x  + (i * multiplier), y: initialPosition.y};
+                if (samePosition(desiredPosition, passedPosition)){
+                    if (this.tileIsEmptyOrOccupiedByEnemy(passedPosition, boardState, team)){
+                        return true;
+                    }                       
+                } else {
+                    if (this.tileIsOccupied(passedPosition, boardState)){
+                        break;
+                    }
+                } 
+            }
+        }
+        return false;
+    }
+
     isValidMove(
         initialPosition: Position,
         desiredPosition: Position,
@@ -145,35 +178,7 @@ export default class Referee {
                 validMove = this.bishopMove(initialPosition, desiredPosition, team, boardState);
                 break;
             case PieceType.ROOK:
-                if (initialPosition.x === desiredPosition.x){
-                    for (let i = 1; i < 8; i++){
-                        let multiplier = (desiredPosition.y < initialPosition.y) ? -1 : 1;
-                        let passedPosition: Position = {x: initialPosition.x, y: initialPosition.y + (i * multiplier)};
-                        if (samePosition(desiredPosition, passedPosition)){
-                            if (this.tileIsEmptyOrOccupiedByEnemy(passedPosition, boardState, team)){
-                                validMove = true
-                            }                       
-                        } else {
-                            if (this.tileIsOccupied(passedPosition, boardState)){
-                                break;
-                            }
-                        } 
-                    }
-                } else if (initialPosition.y === desiredPosition.y){
-                    for (let i = 1; i < 8; i++){
-                        let multiplier = (desiredPosition.x < initialPosition.x) ? -1 : 1;
-                        let passedPosition: Position = {x: initialPosition.x  + (i * multiplier), y: initialPosition.y};
-                        if (samePosition(desiredPosition, passedPosition)){
-                            if (this.tileIsEmptyOrOccupiedByEnemy(passedPosition, boardState, team)){
-                                validMove = true
-                            }                       
-                        } else {
-                            if (this.tileIsOccupied(passedPosition, boardState)){
-                                break;
-                            }
-                        } 
-                    }
-                }
+                validMove = this.rookMove(initialPosition, desiredPosition, team, boardState);
                 break;
             default:
                 validMove = false;
